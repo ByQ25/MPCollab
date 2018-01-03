f6e567a505b83340b2c96ed29ef90a3fa7c73f58
Michał Paluch <michaluspalus@gmail.com>
2018-01-03 21:36:30 +0100

    BitmapFromSource and SourceFromBitmap

--- MPCollab/DTOext.cs
+++ MPCollab/DTOext.cs
@@ -33,7 +33,15 @@ namespace MPCollab
         public DTOext(string text) : this(new MemoryStream(), text, new StringCollection(), null, false) { }
         public DTOext(StringCollection fileDropList) : this(new MemoryStream(), "", fileDropList, null,false) { }
         public DTOext(BitmapSource image) : this(new MemoryStream(), "", new StringCollection(), image, false) { }
-        private DTOext(SerializationInfo info, StreamingContext context) : this((MemoryStream)info.GetValue("Audio", Type.GetType("MemoryStream")), info.GetString("Text"), (StringCollection)info.GetValue("FileDropList", Type.GetType("StringCollection")), (BitmapSource)info.GetValue("Image", Type.GetType("BitmapSource")), info.GetBoolean("Paste")) { }
+        private DTOext(SerializationInfo info, StreamingContext context)
+        {
+            this.audio = (MemoryStream)info.GetValue("Audio", typeof(MemoryStream));
+            this.text = info.GetString("Text");
+            this.fileDropList = (StringCollection)info.GetValue("FileDropList", typeof(StringCollection));
+            this.image = new BitmapImage();
+            this.paste = info.GetBoolean("Paste");
+            this.image = SourceFromBitmap((System.Drawing.Bitmap)info.GetValue("Image", typeof(System.Drawing.Bitmap)));
+        }
 
         public MemoryStream Audio { get { return audio; } }
         public string Text { get { return text; } }
@@ -46,8 +54,40 @@ namespace MPCollab
             info.AddValue("Audio", audio, typeof(MemoryStream));
             info.AddValue("Text", text);
             info.AddValue("FileDropList", fileDropList, typeof(StringCollection));
-            info.AddValue("Image", image, typeof(BitmapSource));
+            //info.AddValue("Image", image, typeof(BitmapSource));
+            info.AddValue("Image", BitmapFromSource(image),typeof(System.Drawing.Bitmap));
             info.AddValue("Paste", paste);
+        }
+
+        private System.Drawing.Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            System.Drawing.Bitmap bitmap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new System.Drawing.Bitmap(outStream);
+            }
+            return bitmap;
+        }
+        private BitmapSource SourceFromBitmap(System.Drawing.Bitmap bitmap)
+        {
+            byte[] array;
+            using (var stream = new MemoryStream())
+            {
+                bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                array = stream.ToArray();
+            }
+
+            var img = new System.Windows.Media.Imaging.BitmapImage();
+            using (MemoryStream outStream = new MemoryStream(array))
+            {
+                img.BeginInit();
+                img.StreamSource = outStream;
+                img.EndInit();
+            }
+            return img;
         }
     }
 }
